32f43f61ee2329a84b317e64854e68741bb40b31
Christopher Harden <Harden.Christopher00@gmail.com>
2021-09-15 10:07:39 -0500

    added apps.js and worked on booleans

---
+++ apps.js
@@ -0,0 +1,21 @@
+
+
+let masterName = prompt ('Is my name Chris?')
+(masterName.toLowerCase());
+if (masterName === 'yes' || masterName === 'y') {alert ('That is correct!')}
+(masterName.toLowerCase());
+else if (masterName === 'no' || masterName === 'n') {alert ('That is Incorrect')}
+console.log(masterName.toLowerCase());
+console.log('The user answered ' + masterName + ' to question number one.')
+
+
+let childCount = prompt ('Do I have two children?');
+if (childCount === 'no' || childCount === 'n') {alert ('That is correct!')}
+else if (childCount === 'yes' || childCount === 'y') {alert ('That is Incorrect')}
+console.log('The user answered ' + childCount + ' to question number two.')
+
+let y =='yes'
+
+var test1 = 
+
+{alert ('That is corret'
